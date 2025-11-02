af830ddf84b355e8e5ac8348041ac4cd13d294b4
Zindvera <zindvera@gmail.com>
2025-11-02 20:49:54 +0530

    Added almost final files

---
+++ redirect.js
@@ -0,0 +1,3 @@
+// This is created for redirect purpose - suppose if this platform successfuly work then 
+// we can redirect all created index.html in wall-art & stickers folder to new SaaS domain url.
+
